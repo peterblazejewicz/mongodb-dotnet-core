96fe0959a71bb1e50519d8d115f2aae9636d7db7
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-08-19 21:26:27 +0200

    Update Untyped example
    - IP based connection to localhost DB
    - code refactorign and cleanup
    - VSCode settigns to launch code

--- MongoDBDotnetCore/src/UntypedDocsExample/Program.cs
+++ MongoDBDotnetCore/src/UntypedDocsExample/Program.cs
@@ -9,16 +9,19 @@ namespace UntypedDocsExample
     {
         public static void Main(string[] args)
         {
-          var client = new MongoClient("mongodb://localhost:27017");
-          var database = client.GetDatabase("test");
-          var collection = database.GetCollection<BsonDocument>("bar");
-          collection.InsertOneAsync(new BsonDocument("Name", "Jack"));
-          var list = collection.Find(new BsonDocument("Name", "Jack")).ToList();
-          Console.WriteLine(list);
-          foreach(var document in list)
-          {
-              Console.WriteLine(document["Name"]);
-          }
+            // you need to use IP for connection on OS X
+            // otherwise timeout is kicking in
+            var client = new MongoClient("mongodb://127.0.0.1:27017");
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<BsonDocument>("bar");
+            collection.InsertOne(new BsonDocument("Name", "Jack"));
+            var list = collection.Find(new BsonDocument("Name", "Jack"))
+              .ToList();
+            foreach (var document in list)
+            {
+                Console.WriteLine(document["Name"]);
+            }
+            Console.WriteLine("Closing ...");
         }
     }
 }
