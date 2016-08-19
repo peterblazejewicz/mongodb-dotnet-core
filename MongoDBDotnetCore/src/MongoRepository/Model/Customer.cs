01041847fcda84c77cc742c21461b0c34f37e55d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-08-19 22:19:43 +0200

    Sample Model docs

---
+++ MongoDBDotnetCore/src/MongoRepository/Model/Customer.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace MongoRepository.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Product> Products { get; set; }
+        public Customer()
+        {
+            this.Products = new List<Product>();
+        }
+    }
+}
